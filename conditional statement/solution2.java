a42afc0e15a48cd1789bc3099a488374c43b1226
Sudipta Panda <163841881+SUDIPTAPANDA61@users.noreply.github.com>
2025-02-15 21:09:35 +0530

    Add files via upload

---
+++ conditional statement/solution2.java
@@ -0,0 +1,12 @@
+public class solution2 {
+    public static void main(String args[]){
+            double temp = 103.5;
+            if(temp>100){
+                System.out.println("you have feever");
+            }
+            else{
+                System.out.println("no fever");
+            }
+}
+    
+}
